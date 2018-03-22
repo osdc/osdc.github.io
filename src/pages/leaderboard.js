8d4ca14b01649977483685dd57f02058d5b0deea
Ujjwal Sharma <usharma1998@gmail.com>
2018-03-22 21:24:21 +0530

    Add a better layout

--- src/pages/leaderboard.js
+++ src/pages/leaderboard.js
@@ -1,5 +1,45 @@
 import React from 'react';
 import Octokit from '@octokit/rest';
+import styled, { injectGlobal } from 'styled-components';
+
+injectGlobal`
+  * {
+    box-sizing: border-box;
+  }
+`;
+
+const Header = styled.h1`
+  text-align: center;
+`;
+
+const ListItem = styled.li`
+  height: 54px;
+  width: 400px;
+  margin: 5px auto;
+  border: 2px solid red;
+  font-size: 1.8rem;
+  display: flex;
+`;
+
+const Avatar = styled.img`
+  height: 40px;
+  width: 40px;
+  border-radius: 50%;
+  margin: 5px;
+`;
+
+const Username = styled.span`
+  flex-grow: 1;
+  padding: 8px;
+  text-align: center;
+`;
+
+const Commits = styled.span`
+  padding: 8px;
+  border-left: 2px solid red;
+  width: 52px;
+  text-align: center;
+`;
 
 function addCommit(prevState, user) {
   if (!user) {
@@ -59,18 +99,16 @@ export default class extends React.Component {
       return b.commits - a.commits;
     });
     const commitList = sorted.map((user, i) => (
-      <li key={i}>
-        <img src={user.avatar} />
-        <a href={user.url} target="_blank">
-          {user.username}
-        </a>{' '}
-        ==> {user.commits}
-      </li>
+      <ListItem key={i}>
+        <Avatar src={user.avatar} />
+        <Username>{user.username}</Username>
+        <Commits>{user.commits}</Commits>
+      </ListItem>
     ));
 
     return (
       <div>
-        <h1>Leaderboard</h1>
+        <Header>Leaderboard</Header>
         <ol>{commitList}</ol>
       </div>
     );
