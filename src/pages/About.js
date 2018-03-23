4667391a849c6914b25ed3e2ff9e039faa4cdc20
Yugandhar Tripathi <tripathi.yugandhar@gmail.com>
2018-03-23 19:52:15 +0530

    added about section

--- src/pages/About.js
+++ src/pages/About.js
@@ -35,5 +35,7 @@ export default() =><Container>
         <Linknormal><Link to="/leaderboard">Community</Link></Linknormal>
         <Linknormal><Link to="/About">About</Link></Linknormal>
       </HeadLinks>
-       
+        <About><u>About Us</u></About>
+        <AboutSection>We are an Open Source Community based in and around Jaypee Institute of Information Technology, Noida, India. A community of web developers, android freaks, machine learning enthusiasts, hackers, designers, game developers and most significantly Explorers. We welcome those who believe in the open source philosophy and are willing to sacrifice their naps in order to change the world.<br></br><br></br>
+We also organise various workshops, talks and hackathons in an effort towards encouraging more people to lean into the open source world! We love having late night conversations on tech and building new things. If you love the same just hop in, we are looking forward for your participation.</AboutSection>
      </Container>
